4336e877158c4884b5887fa0243f625a86db4b04
Brad Waterhouse <Bradley.Waterhouse-CIC@ibm.com>
2021-11-05 09:35:24 +0000

    Names-Displayed

--- testApp/routes/index.js
+++ testApp/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   //Takes an error if any, a http response and the userData in JSON format
   request.get(usersUrl, (error, response, userData) => {
     const parsedData = JSON.parse(userData);
-    res.json(parsedData);
+    res.render('index', {parsedData});
   })
 });
 
